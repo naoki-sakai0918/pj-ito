5038d63e0bc1bee629042df71ccd78740f19c1fb
naoki.sakai <naoki.sakai@insight-lab.co.jp>
2023-08-04 17:34:04 +0900

    bugfix

--- pages/api/sendMessage.js
+++ pages/api/sendMessage.js
@@ -3,12 +3,15 @@ import { WebClient } from '@slack/web-api';
 const web = new WebClient(process.env.SLACK_API_TOKEN);
 
 async function sendMessage(user, message) {
-  const response = await web.chat.postMessage({
-    channel: user,
-    text: message,
-  });
-  console.log(response);
-  res.status(200).send('ok');
+  try {
+    const response = await web.chat.postMessage({
+      channel: user,
+      text: message,
+    });
+    console.log(response);
+  } catch (error) {
+    console.error('Error sending message:', error);
+  }
 }
 
 export default async function handler(req, res) {
@@ -25,9 +28,7 @@ export default async function handler(req, res) {
         let randomNumber;
         do {
           randomNumber = Math.floor(Math.random() * 100) + 1;
-        } while (
-          generatedNumbers.has(randomNumber)
-        );
+        } while (generatedNumbers.has(randomNumber));
         generatedNumbers.add(randomNumber);
         message += randomNumber;
         if (i != stage) {
@@ -35,8 +36,9 @@ export default async function handler(req, res) {
         }
       }
       message += 'です！';
-      sendMessage(user, message);
+      await sendMessage(user, message); // `await`を追加
     }
+    res.status(200).send('ok');
   } catch (err) {
     console.error(err);
     res.status(500).send('Internal Server Error for sendMessage');
